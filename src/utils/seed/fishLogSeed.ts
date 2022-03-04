8af9649c0c93b1aae2565f5179d894d09d801f9d
luiza <luizaesteves987@gmail.com>
2022-03-04 08:37:47 -0300

    removing comments

--- src/utils/seed/fishLogSeed.ts
+++ src/utils/seed/fishLogSeed.ts
@@ -61,8 +61,6 @@ const fishLogSeed = async () => {
       columnToKey,
     });
 
-    //for (let i = 0; i < result.Plan2.length; i += 1) {
-
     let fish = {
       [Symbol.asyncIterator]() {
         return {
@@ -124,67 +122,6 @@ const fishLogSeed = async () => {
   console.log(error);
 }
 
-
-
-
-
-
-  //try {
-  // const fishWiki = await FishWiki.find();
-  // if (fishWiki.length > 0) {
-  //   return;
-  // }
-  // const result: ISheet = await excelToJson({
-  //   sourceFile: 'src/utils/seed/planilha-dados1.xlsx',
-  //   header: {
-  //     rows: 1,
-  //   },
-  //   columnToKey,
-  // });
-
-  // for (let i = 0; i < result.Plan2.length; i += 1) {
-  //   const fish = {
-  //     largeGroup: result.Plan2[i].largeGroup,
-  //     group: result.Plan2[i].group,
-  //     commonName: result.Plan2[i].commonName,
-  //     scientificName: result.Plan2[i].scientificName,
-  //     family: result.Plan2[i].family,
-  //     food: result.Plan2[i].food,
-  //     habitat: result.Plan2[i].habitat,
-  //     maxSize: result.Plan2[i].maxSize,
-  //     maxWeight: result.Plan2[i].maxWeight,
-  //     isEndemicInfo: result.Plan2[i].isEndemicInfo,
-  //     isEndemic: !!(
-  //       result.Plan2[i].isEndemicInfo !== undefined &&
-  //       result.Plan2[i].isEndemicInfo.toLowerCase().includes('sim')
-  //     ),
-  //     isThreatenedInfo: result.Plan2[i].isThreatenedInfo,
-  //     isThreatened: !!(
-  //       result.Plan2[i].isThreatenedInfo !== undefined &&
-  //       result.Plan2[i].isThreatenedInfo.toLowerCase().includes('sim')
-  //     ),
-  //     hasSpawningSeasonInfo: result.Plan2[i].hasSpawningSeasonInfo,
-  //     hasSpawningSeason: !!(
-  //       result.Plan2[i].hasSpawningSeasonInfo !== undefined &&
-  //       result.Plan2[i].hasSpawningSeasonInfo.toLowerCase().includes('sim')
-  //     ),
-  //     wasIntroducedInfo: result.Plan2[i].wasIntroducedInfo,
-  //     wasIntroduced: !!(
-  //       result.Plan2[i].wasIntroducedInfo !== undefined &&
-  //       result.Plan2[i].wasIntroducedInfo.toLowerCase().includes('sim')
-  //     ),
-  //     funFact: result.Plan2[i].funFact,
-  //     photo: result.Plan2[i].photo,
-  //   };
-  // eslint-disable-next-line no-await-in-loop
-  //     await FishWiki.create(fish);
-  //   }
-  //   console.log('Planilha populada com sucesso!');
-
-  // } catch (error) {
-  //   console.log('Não foi possível popular a planilha!');
-  //   console.log(error);
-  //}
 };
 
 export default fishLogSeed;
