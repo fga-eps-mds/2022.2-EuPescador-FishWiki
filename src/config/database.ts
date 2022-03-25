4dc1ffcd48af079d473d2d5047e5d97e947b84b5
Leticia <leticiards.04@gmail.com>
2022-03-24 21:47:58 -0300

    [#76] - Atualização do Docker

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,31 +1,20 @@
-import mongoose from 'mongoose';
+import { Pool } from 'pg'
 import fishLogSeed from '../utils/seed/fishLogSeed';
 
 const databaseConnect = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGODB_URI || 'mongodb://db/wiki',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-      },
-      (err) => {
-        if (!err) {
-          console.log('Conexão estabelecida com sucesso com o MongoDB');
-        } else {
-          console.log(
-            `Falhou a estabelecer a conexão com o MongoDB, falhou com erro: ${err}`
-          );
-        }
-      }
-    );
+    const pool = new Pool({
+      user: '',
+      host: '',
+      database: '',
+      password: '',
+      port: 2
+  });
+  return pool;
     await fishLogSeed();
   } catch (error) {
     console.log('Não foi possível inicicializar corretamente a base de dados!');
     console.log(error);
   }
 };
-
 export default databaseConnect;
