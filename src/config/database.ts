9eb1cf9da5f790472d32fdfa8fe5c9448f75d8af
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-04-11 23:45:00 -0300

    Configure port to deploy

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,7 @@ import { DataSource } from 'typeorm';
 export const connection = new DataSource({
   type: 'postgres',
   host: process.env.POSTGRES_HOST || 'db',
-  port: 5433,
+  port: Number(process.env.POSTGRES_PORT) || 5433,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
