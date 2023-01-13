0489400cf4482e437ea50952bc5d89b321364c71
Weslley Barros <weslley17w@gmail.com>
2023-01-13 00:31:42 -0300

    Corrige erro nas rotas
    Co-authored-by: oo7gabriel <gabrielfsacademico@gmail.com>

--- src/controllers/fishWikiController.ts
+++ src/controllers/fishWikiController.ts
@@ -1,12 +1,12 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import { FishWiki } from '../database/entities/fishWiki';
 import { connection } from '../database';
+import { RequestWithUserRole } from '../Interface/fishLogInterfaces';
 
 export default class FishController {
-  createFish = async (req: Request, res: Response) => {
+  createFish = async (req: RequestWithUserRole, res: Response) => {
     try {
       const fishWikiRepository = connection.getRepository(FishWiki);
-
       const {
         scientificName,
         largeGroup,
@@ -28,7 +28,17 @@ export default class FishController {
         isEndemic,
       } = await req.body;
 
-      if (await fishWikiRepository.findOne({ where: { scientificName } })) {
+      const fish = await fishWikiRepository.findOne({
+        where: { scientificName },
+      });
+
+      if (!req.user?.admin && !req.user?.superAdmin) {
+        return res.status(401).json({
+          message: 'Você não tem permissão para criar esse registro',
+        });
+      }
+
+      if (fish) {
         return res.status(409).json({
           message: 'Essa espécie de peixe já foi cadastrada',
         });
@@ -55,7 +65,8 @@ export default class FishController {
       fishWiki.isEndemic = isEndemic;
 
       await fishWikiRepository.save(fishWiki);
-      return res.status(200).json(req.body);
+
+      return res.status(200).json(fishWiki);
     } catch (error) {
       return res.status(500).json({
         message: 'Falha no sistema ao cadastrar, tente novamente!',
@@ -63,10 +74,9 @@ export default class FishController {
     }
   };
 
-  getAllFish = async (req: Request, res: Response) => {
+  getAllFish = async (req: RequestWithUserRole, res: Response) => {
     try {
       const fishWikiRepository = connection.getRepository(FishWiki);
-
       const entries = Object.entries(req.query);
 
       const nonEmptyOrNull = entries.filter(
@@ -93,10 +103,9 @@ export default class FishController {
     }
   };
 
-  getOneFishWiki = async (req: Request, res: Response) => {
+  getOneFishWiki = async (req: RequestWithUserRole, res: Response) => {
     try {
       const fishWikiRepository = connection.getRepository(FishWiki);
-
       const fishId = req.params.id;
       const fishWiki = await fishWikiRepository.findOne({
         where: { id: fishId },
@@ -115,14 +124,19 @@ export default class FishController {
     }
   };
 
-  deleteFish = async (req: Request, res: Response) => {
+  deleteFish = async (req: RequestWithUserRole, res: Response) => {
     try {
       const fishWikiRepository = connection.getRepository(FishWiki);
-
       const fishId = req.params.id;
       const fishWiki = await fishWikiRepository.findOne({
         where: { id: fishId },
       });
+
+      if (!req.user?.admin && !req.user?.superAdmin) {
+        return res.status(401).json({
+          message: 'Você não tem permissão para deletar esse registro',
+        });
+      }
 
       if (!fishWiki) {
         return res.status(404).json({
@@ -130,29 +144,33 @@ export default class FishController {
         });
       }
 
-      await fishWikiRepository.createQueryBuilder("fishWiki")
-      .delete()
-      .where("id = :id", {id: fishId})
-      .execute();
+      await fishWikiRepository
+        .createQueryBuilder('fishWiki')
+        .delete()
+        .where('id = :id', { id: fishId })
+        .execute();
 
-      return res.status(200).json(fishWiki);
-
+      return res.status(200).json({ message: 'Peixe deletado com sucesso!' });
     } catch (error) {
       return res.status(500).json({
         message: 'Falha ao processar requisição',
       });
     }
-  }
+  };
 
-  updateFish = async (req: Request, res: Response) => {
+  updateFish = async (req: RequestWithUserRole, res: Response) => {
     try {
       const fishWikiRepository = connection.getRepository(FishWiki);
-
       const fishId = req.params.id;
-
       const fishWiki = await fishWikiRepository.findOne({
         where: { id: fishId },
       });
+
+      if (!req.user?.admin && !req.user?.superAdmin) {
+        return res.status(401).json({
+          message: 'Você não tem permissão para atualizar esse registro',
+        });
+      }
 
       if (!fishWiki) {
         return res.status(404).json({
@@ -160,19 +178,18 @@ export default class FishController {
         });
       }
 
-      await fishWikiRepository.createQueryBuilder("fishWiki")
-      .update()
-      .set({...req.body})
-      .where("id = :id", {id: fishId})
-      .execute();
+      await fishWikiRepository
+        .createQueryBuilder('fishWiki')
+        .update()
+        .set({ ...req.body })
+        .where('id = :id', { id: fishId })
+        .execute();
 
-      return res.status(200).json({...fishWiki, ...req.body});
-
+      return res.status(200).json({ ...fishWiki, ...req.body });
     } catch (error) {
       return res.status(500).json({
         message: 'Falha ao processar requisição',
       });
     }
-  }
-
+  };
 }
