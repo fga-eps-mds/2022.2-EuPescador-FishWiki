c8914a1bd6fe44c2f837a64dc0bd77fbfd230ed3
Weslley Barros <weslley17w@gmail.com>
2023-01-26 13:20:17 -0300

    Corrige migrations para paginação por data funcionar corretamente

--- src/database/migrations/1674517327959-AddColumnsCreatedAtAndUpdatedAt.ts
+++ src/database/migrations/1674517327959-AddColumnsCreatedAtAndUpdatedAt.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export default class AddColumnsCreatedAtAndUpdatedAt1674706650739
+export default class AddColumnsCreatedAtAndUpdatedAt1674517327959
   implements MigrationInterface
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "fishWiki" ADD "created_at" TIMESTAMP DEFAULT now()`
+      `ALTER TABLE "fishWiki" ADD "created_at" TIMESTAMP`
     );
     await queryRunner.query(
       `ALTER TABLE "fishWiki" ADD "updated_at" TIMESTAMP DEFAULT now()`
--- src/database/migrations/1674706650739-insertData.ts
+++ src/database/migrations/1674706650739-insertData.ts
@@ -3,7 +3,7 @@ import { readFileSync } from 'fs';
 
 const sql = readFileSync('./src/database/migrations/fishData.sql', 'utf8');
 
-export default class insertData1674517327959 implements MigrationInterface {
+export default class insertData1674706650739 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(sql);
   }
